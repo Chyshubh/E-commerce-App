03904ec82d70452115459fa43d4678b41d8dd974
Shubham Parasar <109840672+Chyshubh@users.noreply.github.com>
2024-12-03 21:57:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import 'dotenv/config'
 import connectDb from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
@@ -8,24 +8,24 @@ import productRouter from './routes/productroute.js';
 import kartRouter from './routes/KartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
-// Initialize the app
+//App Config
 const app = express();
+const port = process.env.PORT || 8000;
 connectDb();
 connectCloudinary();
 
-// Middlewares
+//middlewares
 app.use(express.json());
 app.use(cors());
 
-// API Endpoints
+//api endpoints
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
 app.use('/api/kart', kartRouter);
 app.use('/api/order', orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('API Working');
-});
+app.get('/',(req, res)=>{
+ res.send('API Working');
+})
 
-// Export the app to be used by Vercel
-export default app;
+app.listen(port,()=>console.log(`Server started at PORT: ${port}`))
