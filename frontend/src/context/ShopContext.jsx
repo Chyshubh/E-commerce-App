01110f344b9e257272f3caade50831be4206795a
Shubham Parasar <109840672+Chyshubh@users.noreply.github.com>
2024-12-04 22:01:30 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) => {
 
     const currency = "$"
     const delivery_fee = 10;
-    const backendUrl = "http://localhost:8000";
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [kartItems, setKartItems] = useState({});
